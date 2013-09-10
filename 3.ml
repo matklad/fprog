9ea3df37d6bd18badbb256a32c0da84c77108dde
matklad <aleksey.kladov@gmail.com>
2013-09-11 00:01:12 +0400

    fix indentation

--- 3.ml
+++ 3.ml
@@ -1,7 +1,7 @@
 let rec minlist = function
-| []    -> failwith "empty List"
-| [x]   -> x
-| x::xs -> min x (minlist xs)
+  | []    -> failwith "empty List"
+  | [x]   -> x
+  | x::xs -> min x (minlist xs)
 ;;
 assert(minlist [3; 2; 7;] = 2);
 assert(minlist [1]    = 1);
@@ -9,34 +9,34 @@ assert(minlist [1; 2] = 1);
 assert(minlist [2; 1] = 1);;
 
 let minsum xs =
-    let rec sums = function
+  let rec sums = function
     | x::y::xs -> (x+y)::sums (y::xs)
     | _ -> []
-    in
-    minlist (sums xs)
+  in
+  minlist (sums xs)
 ;;
 assert(minsum [1; 8; 3; 2; 7] = 5);
 assert(minsum [1; 2] = 3);
 assert(minsum [1; 2; 0] = 2)
 
 let rev =
-    let rec go acc = function
+  let rec go acc = function
     | [] -> acc
     | x::xs -> go (x::acc) xs
-    in go []
+  in go []
 ;;
 assert(rev [1;3;7] = [7;3;1])
 
 let rec check cond = function
-| [] -> false
-| x::xs -> cond x || check cond xs
+  | [] -> false
+  | x::xs -> cond x || check cond xs
 ;;
 assert(check (fun x -> x > 5) [3; 2; 7; 4] = true);
 assert(check (fun x -> x < 1) [3; 2; 7; 4] = false)
 
 let rec checkDifferent = function
-| [] -> true
-| x::xs -> not(check ((=)x) xs) && checkDifferent xs
+  | [] -> true
+  | x::xs -> not(check ((=)x) xs) && checkDifferent xs
 ;;
 assert(checkDifferent  [3; 2; 7] = true);
 assert(checkDifferent  [3; 2; 7; 5; 7; 8] = false)
