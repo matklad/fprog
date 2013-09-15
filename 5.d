c8253a4c0bcc6d35362726cb6215ddd31213b168
matklad <aleksey.kladov@gmail.com>
2013-09-15 13:30:21 +0400

    use double instead of real

--- 5.d
+++ 5.d
@@ -2,10 +2,10 @@ module integrator;
 import std.stdio;
 import std.math;
 
-double integral(double delegate(double) fun, double a, double b, int n=100)
+real integral(real delegate(real) fun, real a, real b, int n=100)
 {
-    double h = (b - a) / n;
-    double s = 0;
+    real h = (b - a) / n;
+    real s = 0;
     foreach(i; 0..n-1) {
         s += fun(a + h / 2 + h*i) * h;
     }
@@ -17,7 +17,7 @@ unittest {
     assert(abs(num - 2) < 0.1);
 
     //argh! why I can't just use literal here?
-    double delegate(double) f = x => sin(x);
+    real delegate(real) f = x => sin(x);
     num = integral(f, 0, 2*PI);
     assert(abs(num) < 0.1);
 
