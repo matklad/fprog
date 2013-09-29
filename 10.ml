371687f273a3c7d36ba28c6045454667c57fd4b6
matklad <aleksey.kladov@gmail.com>
2013-09-30 01:23:28 +0400

    fun x fn -> fun y => fun x gn y

--- 10.ml
+++ 10.ml
@@ -8,7 +8,7 @@ assert(powerset [1; 2; 3] = [[]; [1]; [2]; [2;1]; [3]; [3;1]; [3;2]; [3;2;1]])
 ;;
 
 let myreverse ls =
-  [] |> List.fold_right ~f:(fun x fn -> fun y -> x::y |> fn) ~init:Fn.id ls
+  [] |> List.fold_right ~f:(fun x fn y -> x::y |> fn) ~init:Fn.id ls
 ;;
 assert(myreverse [1;2;3] = [3;2;1]);
 assert(myreverse [1] = [1]);
